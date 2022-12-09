70c06ea3689813bc64210fcc536d990031ca14bc
Karolina Jochymek <karolina.jochymek@euvic.pl>
2022-12-09 20:55:54 +0100

    solutions for task 3

--- src/app/modules/shared/shared.module.ts
+++ src/app/modules/shared/shared.module.ts
@@ -1,15 +1,19 @@
 import { CommonModule } from "@angular/common";
 import { NgModule } from "@angular/core";
+import { LowPricePipe } from './pipes/low-price.pipe';
+import { FullStockPipe } from './pipes/full-stock.pipe';
 
 @NgModule({
     declarations: [
-
-    ],
+    LowPricePipe,
+    FullStockPipe
+  ],
     imports: [
       CommonModule,
     ],
     exports: [
-      
+      LowPricePipe,
+      FullStockPipe
     ]
   })
   export class SharedModule { }
