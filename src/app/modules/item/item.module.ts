05810e6e8df74ce3bceaf9d55fdec9fa60aa1864
Karolina Jochymek <karolina.jochymek@euvic.pl>
2022-12-09 20:30:19 +0100

    init commit

--- src/app/modules/item/item.module.ts
+++ src/app/modules/item/item.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from "@angular/core";
 import { FormsModule } from "@angular/forms";
 import { RouterModule } from "@angular/router";
 import { ItemDetailsGuard } from "src/app/services/guards/item-details.guard";
+import { SharedModule } from "../shared/shared.module";
 import { ItemDetailsComponent } from "./item-details/item-details.component";
 import { ItemComponent } from "./unoptimalized-items-list/item/item.component";
 import { UnoptimalizedItemsListComponent } from "./unoptimalized-items-list/unoptimalized-items-list.component";
@@ -18,9 +19,10 @@ import { UnoptimalizedItemsListComponent } from "./unoptimalized-items-list/unop
       FormsModule,
       RouterModule.forChild([
         { path: 'items', component: UnoptimalizedItemsListComponent },
-        { path: 'items/:id', canActivate: [ ItemDetailsGuard ], component: ItemDetailsComponent }]) // guard pozwalajacy na restrykcje dostepu do podstrony 
+        { path: 'items/:id', canActivate: [ ItemDetailsGuard ], component: ItemDetailsComponent }]), // guard pozwalajacy na restrykcje dostepu do podstrony 
         // na bazie wyniku funkcji canActivate (patrz na implementacje guarda) - w tym przypadku routing do strony szczegółów nie zostanie wykonany jesli parametr
         // :id nie będzie cyfrą
-    ]
+      SharedModule
+      ]
   })
   export class ItemModule { }
